4ade4114dda6574ea53e570d79be7d546779242f
Sabri Yaşın <mysql_query@hotmail.com.tr>
2018-12-26 20:01:04 +0300

    Update lotterycode.php

--- lotterycode.php
+++ lotterycode.php
@@ -7,20 +7,5 @@ public function lottery (){
     for($i=0;$i<count( $split ); $i++){
         array_push($uniq,hexdec ( $split[$i])%50 );
     }
-  $sixNumber  = array_chunk(array_unique( $uniq ),6);
-  $data = array();
-  $number ="";
-    for($x=0; $x<count($sixNumber[0]); $x++){
-        if($sixNumber[0][$x] == 0){
-            $sixNumber[0][$x] = $sixNumber[1][0];
-            $number .= sprintf('%02d',$sixNumber[1][0])."-";
-        }else{
-            $number .= sprintf('%02d',$sixNumber[0][$x])."-";
-        }
-    }
-  $data[0] = $number;
-  $data[1] = $hash;
-  $data[2] = $orjinalhash;
-  return $data;
 }
 ?>
