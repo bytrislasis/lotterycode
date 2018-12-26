7bfedcf50d11ad18f55af715318adeca78705402
Sabri Yaşın <mysql_query@hotmail.com.tr>
2018-12-26 20:01:54 +0300

    Update lotterycode.php

--- lotterycode.php
+++ lotterycode.php
@@ -5,7 +5,7 @@ public function lottery (){
   $split = str_split( $hash,4 );
   $uniq = array();
     for($i=0;$i<count( $split ); $i++){
-        array_push($uniq,hexdec ( $split[$i])%50 );
+        array_push($uniq,hexdec ( $split[$i]) % 50 );
     }
 }
 ?>
